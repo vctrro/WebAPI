0cd2a79904edda4dd7f0c53c920f29480d329205
Victor Ro <vctr.ro@gmail.com>
2024-11-12 10:16:50 -0500

    Jast save

--- ContosoPizza/Controllers/PizzaController.cs
+++ ContosoPizza/Controllers/PizzaController.cs
@@ -13,8 +13,21 @@ public class PizzaController : ControllerBase
     }
 
     // GET all action
+    [HttpGet]
+    public ActionResult<List<Pizza>> GetAll() =>
+    PizzaService.GetAll();
 
     // GET by Id action
+    [HttpGet("{id}")]
+    public ActionResult<Pizza> Get(int id)
+    {
+        var pizza = PizzaService.Get(id);
+
+        if (pizza == null)
+            return NotFound();
+
+        return pizza;
+    }
 
     // POST action
 
